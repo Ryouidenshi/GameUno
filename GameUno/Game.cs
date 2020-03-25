5f30ad7e79f971a53413e9d5b8efcdbe6564d8c8
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2020-03-25 10:58:50 +0500

    Добавил класс игры

---
+++ GameUno/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameUno
+{
+    public class Game : MarshalByRefObject
+    {
+
+        public Dictionary<string, bool> Cards { get; set; }
+
+        public Game(Cards cards)
+        {
+            Cards = new Dictionary<string, bool>();
+        }
+
+    }
+}
