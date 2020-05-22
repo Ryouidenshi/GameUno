68c3c87c81c51b63df28b2ca5a69d81581c65b1a
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2020-05-22 17:42:50 +0500

    Add files via upload

---
+++ GameUnoWPF(22.05)/GetImageOnCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GameUnoWPF
+{
+    public class GetImageOnCard
+    {
+        public ImageSource GetCard(Card card, ImageSource imageSouce)
+        {
+            if(card.Picture == "BlackChangeColor")
+                return new BitmapImage(new Uri("CardsWPF/ChangeColor.jpg", UriKind.Relative));
+            if (card.Picture == "BlackChangeTake")
+                return new BitmapImage(new Uri("CardsWPF/ChangeColorAndTake4Cards.jpg", UriKind.Relative));
+            return new BitmapImage(new Uri("CardsWPF/"+card.Picture+".jpg", UriKind.Relative));
+        }
+    }
+}
