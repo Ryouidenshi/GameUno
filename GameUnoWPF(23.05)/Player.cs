5cd2d61afa2b21b19a3d8f09f71f58d160d83119
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2020-05-23 23:47:01 +0500

    Add files via upload

---
+++ GameUnoWPF(23.05)/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameUnoWPF
+{
+    public class Player
+    {
+        public List<Card> CardList = new List<Card>();
+        public string Name;
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public void Take(Card card)
+        {
+            CardList.Add(card);
+        }
+        
+        public void Clear()
+        {
+            CardList.Clear();
+        }
+    }
+}
