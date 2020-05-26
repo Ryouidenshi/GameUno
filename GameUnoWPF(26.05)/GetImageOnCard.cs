fa55783b5b5a7b6bb683fba680f51bb44971ee80
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2020-05-26 19:21:18 +0500

    Add files via upload

---
+++ GameUnoWPF(26.05)/GetImageOnCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GameUnoWPF
+{
+    public class GetImageOnCard
+    {
+        public ImageSource GetCard(Card card, ImageSource imageSouce)
+        {
+            return new BitmapImage(new Uri("CardsWPF/" + card.Picture + ".jpg", UriKind.Relative));
+        }
+    }
+}
---
+++ GameUnoWPF(26.05)/Rules.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameUnoWPF
+{
+    public class Rules
+    {
+        readonly Player player;
+        public Rules(Player player)
+        {
+            this.player = player;
+        }
+
+        public List<Card> GetMarkOnCards(Deck deck, Card cardOnMap, Turn turn)
+        {
+            var listCardPlayer = player.CardList;
+            for (int i=0;i<listCardPlayer.Count;i++)
+            {              
+                if (cardOnMap.Color == listCardPlayer[i].Color || cardOnMap.Score == listCardPlayer[i].Score
+                    || cardOnMap.Active == listCardPlayer[i].Active)
+                    listCardPlayer[i].Mark = true;
+                else listCardPlayer[i].Mark = false;
+            }
+            return listCardPlayer;
+        }
+
+        public void GetPropertiesSelectCard(string activeSelectCard, Turn turn)
+        {
+            if (activeSelectCard == "ChangeDirection")
+                turn.ChangeDirection = true;
+            if (activeSelectCard == "Skip")
+                turn.SkipMove = true;
+            if (activeSelectCard == "TakeTwoCards")
+                turn.Take2Cards = true;
+        }
+    }
+}
