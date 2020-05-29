379dc2c78b8472e97b8cbc2792b5d050a411af92
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2020-05-30 00:43:43 +0500

    Add files via upload

---
+++ UNOGAME(30.05)/UNOGAME/ViewModels/Info.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using UNOGAME.Models;
+
+namespace UNOGAME.ViewModels
+{
+    static class Info
+    {
+        public static string PlayerName = "Jhon";
+        public static ImageSource GetCardPicture(string picture)
+        {
+            return new BitmapImage(new Uri("../Views/CardsWPF/" + picture + ".jpg", UriKind.Relative));
+        }
+        public static BoardInfo boardInfo = new BoardInfo(new List<Player>(),new List<Card>());
+        public static UnoGame game; 
+        public static void Update()
+        {
+            game.PlayersCardsCount = new string[]
+            {
+                boardInfo.PlayersCardsCounter[1],
+                boardInfo.PlayersCardsCounter[2],
+                boardInfo.PlayersCardsCounter[3]
+            };
+            game.PlayerNames = new string[] 
+            { 
+                boardInfo.PlayersNames[0],
+                boardInfo.PlayersNames[1],
+                boardInfo.PlayersNames[2],
+                boardInfo.PlayersNames[3]
+            };
+        }
+    }
+}
---
+++ UNOGAME(30.05)/UNOGAME/ViewModels/UnoGame.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UNOGAME.Models;
+
+namespace UNOGAME.ViewModels
+{
+    class UnoGame : ViewModelBase
+    {
+        public ICommand Command
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    Info.game = this;
+                    Info.Update();
+                });
+            }
+            
+        }
+        // Имена игроков
+        private string[] playerNames;
+        public string[] PlayerNames
+        {
+            get { return playerNames; }
+            set
+            {
+                playerNames = value;
+                RaisePropertyChanged(() => PlayerNames);
+            }
+        } 
+        // Кол-во карт других игроков
+        private string[] playersCardsCount;
+        public string[] PlayersCardsCount
+        {
+            get { return playersCardsCount; }
+            set
+            {
+                playersCardsCount = value;
+                RaisePropertyChanged(() => PlayersCardsCount);
+            }
+        }
+        
+    }
+}
