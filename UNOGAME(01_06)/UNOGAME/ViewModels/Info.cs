38cc1b7746152f9e7f3220c8a1fb4532e7a04e0c
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2020-06-01 00:45:26 +0500

    Add files via upload

---
+++ UNOGAME(01_06)/UNOGAME/ViewModels/Info.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using UNOGAME.Models;
+
+namespace UNOGAME.ViewModels
+{
+    static class Info
+    {
+        public static string winner;
+        public static string PlayerName = "Jhon";
+        public static BoardInfo boardInfo;
+        public static UnoGame game;
+        public static int currentCard = -1;
+        public static bool mark = false;
+        public static string[] borders;
+        public static string[] visibleCards;
+        public static bool[] enabledCards;
+        public static MediaPlayer media;
+        public static ImageSource GetCardPicture(string picture)
+        {
+            return new BitmapImage(new Uri("CardsWPF/" + picture + ".jpg", UriKind.Relative));
+        }
+        public static void Update()
+        {
+            if(boardInfo.centralCard != null)   
+                game.CentralCard = GetCardPicture(boardInfo.centralCard.Picture);
+            game.PlayersCardsCount = boardInfo.PlayersCardsCounter.ToArray();
+            game.PlayerNames = boardInfo.PlayersNames.ToArray();
+            var images = new List<ImageSource>();
+            foreach (var img in boardInfo.Pictures)
+                images.Add(GetCardPicture(img));
+            game.ButtonPictures = images.ToArray();
+            game.CardBorders = borders;
+            game.VisibleCards = visibleCards;
+            game.EnabledCards = enabledCards;
+        }
+    }
+}
---
+++ UNOGAME(01_06)/UNOGAME/Views/WinnerWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace UNOGAME.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class WinnerWindow : Window
+    {
+        public WinnerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowLables()
+        {
+            var rnd = new Random();
+            switch (rnd.Next(4))
+            {
+                case 1:
+                    first.Visibility = Visibility.Visible;
+                    third.Visibility = Visibility.Visible;
+                    second.Visibility = Visibility.Hidden;
+                    fourth.Visibility = Visibility.Hidden;
+                    break;
+                case 2:
+                    second.Visibility = Visibility.Visible;
+                    third.Visibility = Visibility.Visible;
+                    first.Visibility = Visibility.Hidden;
+                    fourth.Visibility = Visibility.Hidden;
+                    break;
+                case 3:
+                    fourth.Visibility = Visibility.Visible;
+                    third.Visibility = Visibility.Visible;
+                    second.Visibility = Visibility.Hidden;
+                    first.Visibility = Visibility.Hidden;
+                    break;
+                case 4:
+                    first.Visibility = Visibility.Visible;
+                    third.Visibility = Visibility.Visible;
+                    second.Visibility = Visibility.Visible;
+                    fourth.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            gif.Position = TimeSpan.FromMilliseconds(1);
+            gif.Play();
+            ShowLables();
+        }
+    }
+}
